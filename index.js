e238281621eacb70e6e9383b814b628a515a754e
Curtis Arnold <arnold.curtis@gmail.com>
2019-06-08 00:56:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,12 +15,13 @@ function logWhisper (string) {
 }
 
 function sayHiToGrandma (string) {
-  if string == ""
-  
-  if string.toUpperCase() === string {
+  if (string == "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+  else if (string.toUpperCase() === string) {
     return "YES INDEED!"; 
   }
-  if string.toLowerCase() === string {
+  else if (string.toLowerCase() === string) {
     return "I can't hear you!";
   }
 }
